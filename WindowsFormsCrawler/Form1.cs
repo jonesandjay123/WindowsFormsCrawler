bfae53b4dd8181e55391754a97d194bb8ca04ea3
Jones <jonesandjay123@gmail.com>
2018-02-23 13:18:08 +0800

    加入專案檔案。

---
+++ WindowsFormsCrawler/Form1.cs
@@ -0,0 +1,120 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsCrawler
+{
+    public partial class Form1 : Form
+    {
+        int progress = 0;
+        Thread Crawler = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Text = "測試文字";
+
+            if (string.IsNullOrEmpty(textBox1.Text)) {
+                //先自動做成魔物獵人
+                textBox1.Text = "https://forum.gamer.com.tw/B.php?bsn=5786";
+            }
+            string URL = textBox1.Text.Trim();
+            /*
+            # region=== 進度條的顯示===
+            int maxCount = 100;
+
+            this.Invoke((MethodInvoker)delegate
+            {
+                progressBar1.Maximum = maxCount;
+
+            });
+
+
+            for (int i = 0; i<= maxCount; i++)
+            {
+                Console.WriteLine("=========i:" + i + "=============");
+
+                progress++;
+                progressBar1.Value = (int)((float) i / progressBar1.Maximum * 100);
+                this.Text = "目前進度... " + (int)((float) i / progressBar1.Maximum * 100) + " %";
+
+                //停一秒
+                Thread.Sleep(100);
+            }
+            #endregion
+            */
+
+
+            #region ===HtmlAgilityPack爬蟲===
+            //HtmlAgilityPack官方教學: http://html-agility-pack.net/?z=codeplex
+            HtmlWeb webClient = new HtmlWeb();
+            var doc = webClient.Load(URL);
+
+            //var head = doc.DocumentNode.SelectSingleNode("//head/title");
+            //Console.WriteLine("Node Name: " + head.Name + "\n" + head.OuterHtml);
+       
+            var table = doc.DocumentNode.SelectSingleNode("//body//table");
+            //Console.WriteLine("table.ChildNodes.Count: " + table.FirstChild.NextSibling.Attributes["class"]);
+
+            var tr = doc.DocumentNode.SelectNodes("//body//tr[contains(@class,'b-list__row b-list__row--sticky')]").First(); //找到符合條件的第一個tr
+            Console.WriteLine("tr: " + tr.ChildNodes.Count);
+
+            var tr0 = doc.DocumentNode.SelectNodes("//body//td[contains(@class,'b-list__main')]").First(); //找到符合條件的第一個tr
+            Console.WriteLine("tr0: " + tr0.InnerHtml);
+            var hrefstring = tr0.FirstChild.Attributes["href"].Value;
+            Console.WriteLine("hrefstring: " + hrefstring);
+
+
+            //Console.WriteLine("td: " + td.Attributes["class"].Value);
+
+            /*
+            var t1 = doc.DocumentNode.SelectSingleNode("//div[@id='BH-master']").InnerText;
+            Console.WriteLine("body: " + t1);
+            */
+
+            /*
+            var link = doc.DocumentNode.SelectSingleNode("//body//a[contains(@class,'data-gtm')][0]");
+            var hrefstring = link.Attributes["href"].Value;
+            */
+
+            /*
+            //根據HTML節點NODE的ID獲取節點
+            HtmlNode navNode = doc.GetElementbyId("BH-master");
+            //根據XPATH來索引節點
+            //div[2]表示文章鏈接a位於post_list裡面第3個div節點中
+            HtmlNode navNode2 = navNode.SelectSingleNode("//table[@class='b-list']");
+            HtmlNode navNode3 = navNode.SelectSingleNode("//table[@id='loads']//tbody//tr");
+
+            string articleTitle = navNode.InnerText;
+            */
+
+            /*
+            //獲取文章鏈接地址
+            string articleTitle = navNode3.Attributes["href"].Value.ToString();
+            //獲取文章標題
+            string articleName = navNode2.InnerText;
+            */
+
+
+            //Console.WriteLine("href: " + articleTitle);
+
+            //doc.DocumentNode.SelectSingleNode("//body//a[contains(@class,'b-list')][2]");
+            #endregion
+
+            //c# 分詞索引: http://blog.darkthread.net/post-2017-11-14-lucene-net-notes-3.aspx
+
+            MessageBox.Show("程式結束!");
+        }
+    }
+}
---
+++ WindowsFormsCrawler/Program.cs
@@ -0,0 +1,107 @@
+﻿using Lucene.Net.Analysis;
+using Lucene.Net.Analysis.MMSeg;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Documents;
+using Lucene.Net.Index;
+using Lucene.Net.QueryParsers;
+using Lucene.Net.Search;
+using Lucene.Net.Store;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormsCrawler
+{
+    static class Program
+    {
+        /// <summary>
+        /// 應用程式的主要進入點。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {         
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            
+            
+            /*
+            //AnalyzerTest("盤古分詞", "D:\\PanGuIndex", new PanGuAnalyzer());
+            AnalyzerTest("標準分詞", "D:\\StdAnalyzerIndex",
+                new StandardAnalyzer(Lucene.Net.Util.Version.LUCENE_30));
+            AnalyzerTest("MMSeg MaxWord", "D:\\MMSegIndex", new MMSegAnalyzer());
+            AnalyzerTest("MMSeg Simple", "D:\\MMSegSimpIndex",
+                new Lucene.Net.Analysis.MMSeg.SimpleAnalyzer());
+            AnalyzerTest("MMSeg Complex", "D:\\MMSegCompIndex",
+                new Lucene.Net.Analysis.MMSeg.ComplexAnalyzer());
+            AnalyzerTest("CWSharp詞庫分詞", "D:\\CWStdIndex",
+                new CwsAnalyzer(
+                new Yamool.CWSharp.StandardTokenizer(
+                    new FileStream("cwsharp.dawg", FileMode.Open))));
+            AnalyzerTest("CWSharp一元分詞", "D:\\CWUniIndex",
+                new CwsAnalyzer(new UnigramTokenizer()));
+            AnalyzerTest("CWSharp二元分詞", "D:\\CWBiIndex",
+                new CwsAnalyzer(new BigramTokenizer()));
+            Console.Read();
+            */
+        }
+
+
+        //http://blog.darkthread.net/post-2017-11-14-lucene-net-notes-3.aspx
+        public static void AnalyzerTest(string title, string indexPath, Analyzer analyzer)
+        {
+            //指定索引資料儲存目錄
+            var fsDir = FSDirectory.Open(indexPath);
+
+            //建立IndexWriter
+            using (var idxWriter = new IndexWriter(fsDir, //儲存目錄
+                analyzer,
+                true, //清除原有索引，重新建立
+                IndexWriter.MaxFieldLength.UNLIMITED //不限定欄位內容長度
+            ))
+
+            {
+                //示範為兩份文件建立索引
+                var doc = new Document();
+                //每份文件有兩個Field: Source、Word
+                doc.Add(new Field("Word",
+                    "生活就像一盒巧克力，你永遠也不會知道你將拿到什麼。",
+                    Field.Store.YES, Field.Index.ANALYZED));
+                idxWriter.AddDocument(doc);
+
+                //建立索引
+                idxWriter.Commit();
+                idxWriter.Optimize();
+            }
+
+
+            var searcher = new IndexSearcher(fsDir, true);
+            //指定欄位名傳入參數
+            QueryParser qp = new QueryParser(Lucene.Net.Util.Version.LUCENE_30,"Word", analyzer);
+
+            Action<string> testQuery = (kwd) =>
+            {
+                var q = qp.Parse(kwd);
+                var hits = searcher.Search(q, 10);
+                Console.WriteLine($"查詢「{kwd}」找到{hits.TotalHits}筆");
+            };
+            Console.WriteLine($"{title}測試");
+            testQuery("魔物獵人");
+            testQuery("要玩銃槍");
+            testQuery("生活");
+            testQuery("就像");
+            testQuery("一盒");
+            testQuery("巧克力");
+            testQuery("永遠");
+            testQuery("不會知道");
+            testQuery("拿到");
+            testQuery("什麼");
+            Console.WriteLine("========================================");
+        }
+        
+
+
+
+    }
+}
